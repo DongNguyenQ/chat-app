9c6e4e2f007e441333fa6e24c3bbf552efaafc7a
Bluez <dongdev@gmail.com>
2021-07-11 16:51:01 +0700

    Add app badger notification number

--- lib/service/push_notification_service.dart
+++ lib/service/push_notification_service.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_app_badger/flutter_app_badger.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+
+// https://www.freecodecamp.org/news/local-notifications-in-flutter/
 class PushNotificationService {
   static FirebaseMessaging _fcm = FirebaseMessaging.instance;
   static FlutterLocalNotificationsPlugin _localNotification
@@ -32,7 +35,11 @@ class PushNotificationService {
     FirebaseMessaging.onMessage.listen(_firebaseNotificationHandler);
 
     // Hanlde when user click on noti
-    FirebaseMessaging.onMessageOpenedApp.listen(_firebaseNotiActionHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('USER CLICK');
+      FlutterAppBadger.removeBadge();
+    });
+
   }
 
   static Future<String?> _getToken() async {
@@ -43,9 +50,11 @@ class PushNotificationService {
     print('HANDLING MESSAGE : ${message.notification!.body} \n '
         'TITLE : ${message.notification!.title} \n '
         'URL : ${message.notification!.android!.imageUrl}');
+    FlutterAppBadger.updateBadgeCount(1);
     RemoteNotification notification = message.notification!;
     AndroidNotification android = message.notification!.android!;
     print('SHOW LOCAL NOTI');
+    // _localNotification.
     _localNotification.show(
         notification.hashCode,
         notification.title,
@@ -67,6 +76,7 @@ class PushNotificationService {
   static void _firebaseNotiActionHandler(RemoteMessage message) {
     print('USER CLICKED ON MESSAGE');
     // return null;
+    FlutterAppBadger.removeBadge();
   }
 
 }
